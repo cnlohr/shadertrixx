a4db7a3f4714264c82f1e3ac2dd356617c23e876
cnlohr <lohr85@gmail.com>
2021-11-06 04:22:11 -0700

    Minor bump in shadertrixx, adding rotating cylinder.

---
+++ Assets/cnlohr/Scripts/TimeOfDay/TimeOfDay.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using BrokeredUpdates;
+
+namespace cnlohr
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class TimeOfDay : UdonSharpBehaviour
+	{
+		public Light lightToControl;
+		private bool bHeld;
+		
+		void Start()
+		{
+			
+		}
+		
+		override public void OnPickup ()
+		{
+			bHeld = true;
+		}
+		
+		override public void OnDrop()
+		{
+			bHeld = false;
+		}
+
+		void Update()
+		{
+			if( !bHeld )
+			{
+				transform.localRotation *= Quaternion.Euler((float)(Time.deltaTime),0,0);
+			}
+			//gameObject.GetComponent<BrokeredSync>()._SendMasterMove();
+			lightToControl.transform.localRotation = transform.localRotation;
+		}
+	}
+}
