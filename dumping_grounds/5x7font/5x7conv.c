d97071b26ac5dcd32a5621f30e811e3ff698b041
cnlohr <lohr85@gmail.com>
2021-11-19 01:38:01 -0500

    Bump

--- dumping_grounds/5x7font/5x7conv.c
+++ dumping_grounds/5x7font/5x7conv.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdint.h>
 
+char linebuffer[8193];
+
+char * lreadline( FILE * f )
+{
+	int c;
+	int pl = 0;
+	while( ( c == fgetc() ) != EOF )
+	{
+		if( pl < sizeof( linebuffer ) - 1 ) 
+			linebuffer[pl++] = c;
+		if( c == '\n' ) break;
+	}
+	linebuffer[pl] = 0;
+	return linebuffer;
+}
+
 int main()
 {
 	FILE * f = fopen( "5x7_cnl.pgm", "rb" );
 	int w, h;
 	{
-		char dump[1024];
-		sscanf( f, "%s\n", dump );
-		sscanf( f, "%s\n", dump );
+		lreadline( f );
+		lreadline( f );
 		sscanf( f, "%d %d\n", &w, &h );
-		sscanf( f, "%s\n", dump );
+		lreadline( f );
 	}
 	if( w != 768 || h != 8 )
 	{
@@ -33,8 +48,18 @@ int main()
 			v |= (buffer_in[x+(bit)*w]?1:0)<<bit;
 		buffero[i] = v;
 	}
-	FILE * cgo = fopen( "shader_header.glsl", "r" ); 
+	FILE * cgo = fopen( "shader_tailer.glsl", "r" ); 
 	FILE * out = fopen( "shader5x7.glsl", "w" );
+	fprintf( out, "
+	for( i = 0 ; i < 141; i++ )
+	{
+		
+	}
+	while( !feof( cgo ) )
+	{
+		fputs( out, lreadline( cgo ) );
+	}
 	
+
 }
 
